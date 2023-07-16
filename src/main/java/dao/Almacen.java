4e43ad00521123e667fd200b9e2dc7a879cc4778
LeslieSanz <114875034+LeslieSanz@users.noreply.github.com>
2023-07-16 00:20:43 -0500

    Agregando form de categoria

---
+++ src/main/java/dao/Almacen.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+import modelo.Producto;
+import util.Conexion;
+
+/**
+ *
+ * @author lesly
+ */
+public class Almacen {
+    //lista categorias con cantidad de productos por cada una
+    public List<Producto> lisCategoria(){
+    List<Producto> lis=new ArrayList();
+    Connection cn=Conexion.getConexion();
+    try{
+        String sql="{call sp_cantCategoria}";
+        CallableStatement st=cn.prepareCall(sql);
+        ResultSet rs=st.executeQuery();
+        while(rs.next()){
+            Producto c=new Producto();
+            c.setNomcat(rs.getString(1));
+            c.setCantcat(rs.getString(2));
+            lis.add(c);        }
+    }catch(Exception ex){
+        ex.printStackTrace();
+    }
+    return lis;
+    }
+}
---
+++ src/main/java/modelo/Producto.java
@@ -0,0 +1,68 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+/**
+ *
+ * @author lesly
+ */
+public class Producto {
+    //producto
+    private String codpro;
+    private String despro;
+    private String stkpro;
+    
+    
+    //categoria
+    private String nomcat;
+    private String cantcat;
+    
+    
+    //ubicacion
+
+    
+    
+    //getters y setters
+    public String getNomcat() {
+        return nomcat;
+    }
+
+    public void setNomcat(String nomcat) {
+        this.nomcat = nomcat;
+    }
+
+    public String getCantcat() {
+        return cantcat;
+    }
+
+    public void setCantcat(String cantcat) {
+        this.cantcat = cantcat;
+    }
+
+    public String getCodpro() {
+        return codpro;
+    }
+
+    public void setCodpro(String codpro) {
+        this.codpro = codpro;
+    }
+
+    public String getDespro() {
+        return despro;
+    }
+
+    public void setDespro(String despro) {
+        this.despro = despro;
+    }
+
+    public String getStkpro() {
+        return stkpro;
+    }
+
+    public void setStkpro(String stkpro) {
+        this.stkpro = stkpro;
+    }
+
+}
